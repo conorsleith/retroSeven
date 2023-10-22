7d8b1983bc660489110e0faeb3fdbf9db5b5de05
Conor <c.conor.sleith@gmail.com>
2023-10-22 11:19:36 -0400

    before I refactor the Activity struct

--- retroSeven/StravaData.swift
+++ retroSeven/StravaData.swift
@@ -8,25 +8,135 @@
 import Foundation
 import Combine
 
-struct StravaActivity: Codable {
+struct Athlete: Decodable {
     let id: Int
-    let name: String
-    let type: String
-    let startDate: Date
-    let distance: Double
-    let movingTime: Int
-    let averageSpeed: Double
-    let maxSpeed: Double
-
+    let resourceState: Int
+    
     enum CodingKeys: String, CodingKey {
         case id
-        case name
-        case type
-        case startDate = "start_date"
-        case distance
-        case movingTime = "moving_time"
+        case resourceState = "resource_state"
+    }
+}
+
+struct Map: Decodable {
+    let id: String
+    let resourceState: Int
+    let summaryPlotline: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case resourceState = "resource_state"
+        case summaryPlotline = "summary_plotline"
+    }
+}
+
+struct StravaActivity: Decodable {
+    let achievementCount: Int
+    let athlete: Athlete
+    let athleteCount: Int
+    let averageCadence: Double
+    let averageHeartrate: Double
+    let averageSpeed: Double
+    let averageTemp: Int
+    let commentCount: Int
+    let commute: Bool
+    let displayHideHeartrateOption: Bool
+    let distance: Double
+    let elapsedTime: Int
+    let elevHigh: Double
+    let elevLow: Double
+    let endLatlng: [Double]
+    let externalId: String
+    let flagged: Bool
+    let fromAcceptedTag: Bool
+    let gearId: String
+    let hasHeartrate: Bool
+    let hasKudoed: Bool
+    let heartrateOptOut: Bool
+    let id: Int
+    let kudosCount: Int
+    let locationCity: String?
+    let locationCountry: String?
+    let locationState: String?
+    let manual: Bool
+    let map: Map
+    let maxHeartrate: Double
+    let maxSpeed: Double
+    let movingTime: Int
+    let name: String
+    let photoCount: Int
+    let prCount: Int
+    let isPrivate: Bool
+    let resourceState: Int
+    let sportType: String
+    let startDate: String
+    let startDateLocal: String
+    let startLatlng: [Double]
+    let sufferScore: Double
+    let timezone: String
+    let totalElevationGain: Double
+    let totalPhotoCount: Int
+    let isTrainer: Bool
+    let type: String
+    let uploadId: Int
+    let uploadIdStr: String
+    let utcOffset: Double
+    let visibility: String
+    let workoutType: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case achievementCount = "achievement_count"
+        case athlete
+        case athleteCount = "athlete_count"
+        case averageCadence = "average_cadence"
+        case averageHeartrate = "average_heartrate"
         case averageSpeed = "average_speed"
+        case averageTemp = "average_temp"
+        case commentCount = "comment_count"
+        case commute
+        case displayHideHeartrateOption = "display_hide_heartrate_option"
+        case distance
+        case elapsedTime = "elapsed_time"
+        case elevHigh = "elev_high"
+        case elevLow = "elev_low"
+        case endLatlng = "end_latlng"
+        case externalId = "external_id"
+        case flagged
+        case fromAcceptedTag = "from_accepted_tag"
+        case gearId = "gear_id"
+        case hasHeartrate = "has_heartrate"
+        case hasKudoed = "has_kudoed"
+        case heartrateOptOut = "heartrate_opt_out"
+        case id
+        case kudosCount = "kudos_count"
+        case locationCity = "location_city"
+        case locationCountry = "location_country"
+        case locationState = "location_state"
+        case manual
+        case map
+        case maxHeartrate = "max_heartrate"
         case maxSpeed = "max_speed"
+        case movingTime = "moving_time"
+        case name
+        case photoCount = "photo_count"
+        case prCount = "pr_count"
+        case isPrivate = "private"
+        case resourceState = "resource_state"
+        case sportType = "sport_type"
+        case startDate = "start_date"
+        case startDateLocal = "start_date_local"
+        case startLatlng = "start_latlng"
+        case sufferScore = "suffer_score"
+        case timezone
+        case totalElevationGain = "total_elevation_gain"
+        case totalPhotoCount = "total_photo_count"
+        case isTrainer = "trainer"
+        case type
+        case uploadId = "upload_id"
+        case uploadIdStr = "upload_id_str"
+        case utcOffset = "utc_offset"
+        case visibility
+        case workoutType = "workout_type"
     }
 }
 
@@ -36,6 +146,7 @@ class StravaDataViewModel: ObservableObject {
     @Published var isLoading: Bool = false
     @Published var error: Error?
     private var cancellables: Set<AnyCancellable> = []
+    private var storedData: Data = Data()
 
     
     func fetchStravaActivities() {
@@ -84,16 +195,33 @@ class StravaDataViewModel: ObservableObject {
             URLSession.shared.dataTaskPublisher(for: request)
                 .map(\.data)
                 .tryMap { data in
-                    if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
-                        print(json)
-                    } else {
-                        let json = JSONSerialization.jsonObject(with: data)
-                        print(data)
+                        // Decode the data into a JSON object (dictionary)
+                        do {
+                            let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                            var singleData: Data = Data()
+                            if let jsonArray = jsonObject as? [[String: Any]] {
+                                // You can now inspect the JSON dictionary
+                                do {
+                                    let _singleData = try JSONSerialization.data(withJSONObject: jsonArray[0], options: [])
+                                    singleData = _singleData
+                                    // Now 'data' contains the JSON representation of the dictionary
+                                } catch {
+                                    print("Error converting dictionary to data: \(error)")
+                                }
+                                do {
+                                    let decodedObject = try JSONDecoder().decode(StravaActivity.self, from: singleData)
+                                    print("we did it!")
+                                } catch {
+                                    print("Error decoding data into object: \(error)")
+                                }
+                                return data
+                            } else {
+                                throw NSError(domain: "YourAppDomain", code: 1, userInfo: ["message": "Failed to cast JSON as [String: Any]"])
+                            }
+                        } catch {
+                            throw error
+                        }
                     }
-                    return data
-                }
-                .decode(type: [StravaActivity].self, decoder: JSONDecoder())
-                .receive(on: DispatchQueue.main)
                 .sink(receiveCompletion: { result in
                     switch result {
                     case .finished:
@@ -102,9 +230,22 @@ class StravaDataViewModel: ObservableObject {
                         self.isLoading = false
                         self.error = error
                     }
-                }, receiveValue: { activities in
-                    self.activities = activities
-                    self.isLoading = false
+                }, receiveValue: { data in
+                    // Store the data for inspection
+                    self.storedData = data
+
+                    // You can also inspect the data here if needed
+                    // print(String(data: data, encoding: .utf8))
+
+                    // Continue decoding and updating your model as needed
+                    do {
+                        let activities = try JSONDecoder().decode([StravaActivity].self, from: data)
+                        self.activities = activities
+                    } catch {
+                        // Handle decoding errors
+                        self.isLoading = false
+                        self.error = error
+                    }
                 })
                 .store(in: &cancellables)
             print("Okay, did a thing")
