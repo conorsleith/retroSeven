53638ab071057a1e3ded4174d104e6d74d788657
Conor <c.conor.sleith@gmail.com>
2023-10-26 14:07:21 -0400

    added watch target

---
+++ retroSevenWatch Watch App/retroSevenWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  retroSevenWatchApp.swift
+//  retroSevenWatch Watch App
+//
+//  Created by Conor Sleith on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct retroSevenWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
